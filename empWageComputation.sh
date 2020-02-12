a0bd3433bf631d8121d53c6faa21eaaa9c5e8b6d
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-02-12 18:59:04 +0530

    [Saad] Refactor. calculating daily employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+wagePerHour=20
+fullDayHour=8
+calculateFullDayWage(){
+	#echo "$((wagePerHour*fullDayHour))"
+	return $((wagePerHour*fullDayHour))
+}
+
+calculateFullDayWage
+fullDayWage=$?
+echo "$fullDayWage"
