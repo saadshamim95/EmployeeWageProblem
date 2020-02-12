70a9cd1c1133733fd9321eba90501d277b72587e
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-02-12 18:27:17 +0530

    [Saad] Refactor. Adding useCase1-AttendanceCheck

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+attendance=$$
+checkAttendance(){
+	attendance=$((RANDOM%2))
+	if((attendance==0))
+	then
+		echo "Employee is absent"
+	else
+		echo "Employee is present"
+	fi
+}
+
+checkAttendance
