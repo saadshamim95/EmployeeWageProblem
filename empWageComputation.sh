4e9c6debabb61e6262fc64f1a120ec22b0ca5bf1
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-02-12 19:23:02 +0530

    [Saad] Refactor. calculating part time wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+wagePerHour=20
+partTimeHour=4
+calculatePartTimeWage(){
+	#echo "$((wagePerHour*partTimeHour))"
+	return $((wagePerHour*partTimeHour))
+}
+
+calculatePartTimeWage
+partTimeWage=$?
+echo $partTimeWage
