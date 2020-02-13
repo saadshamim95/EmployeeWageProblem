3898c8c79b25f4eeab85a9a4c8d2848803092d54
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-02-13 15:38:51 +0530

    [Saad] Refactor. adding daily wage storing functionality

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,37 @@
 #!/bin/bash
+declare -A dailyWage
+Attendance(){
+	if(( $((RANDOM%2))==0 ))
+	then
+		return 0
+	else
+		return 1
+	fi
+}
+
+totalWages=0
+count=0
+
+geWorkingHours(){
+	wage=$1
+	while((count!=20))
+	do
+		Attendance
+		result=$?
+		if(($result==1))
+		then
+			dailyWage[$count]=$wage
+		else
+			dailyWage[$count]=0
+		fi
+		count=$((count+1))
+	done
+}
+
+geWorkingHours 160
+
+echo "Daily Wage"
+for((i=0;i<20;i++))
+do
+	echo -n "${dailyWage[$i]} "
+done
