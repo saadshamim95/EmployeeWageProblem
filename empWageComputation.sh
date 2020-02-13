23c1cdb8a0ea3c3451da57f093696d95d90d5e0a
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-02-13 10:29:38 +0530

    [Saad] Refactor. Adding case statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+calculateFullDayWage(){
+	return 160
+}
+
+calculatePartTimeWage(){
+	return 80
+}
+
+echo "Enter the type of Employement"
+echo "1. Full Time"
+echo "2. Part Time"
+read choice
+case $choice in
+1) 	calculateFullDayWage
+	result=$?
+	echo $((result))
+	;;
+2)	calculatePartTimeWage
+	result=$?
+	echo $((result))
+	;;
+*)	echo "Not a valid Input"
+esac
