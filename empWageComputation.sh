e4202e07ad5cfda0d4a60d263fddc1ef9633816e
Saad Shamim <37002889+saadshamim95@users.noreply.github.com>
2020-02-13 17:23:21 +0530

    [Saad] Refactor. printing day report

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,37 @@
 #!/bin/bash
+declare -a dailyWage
+Attendance(){
+	if(( $((RANDOM%2))==0 ))
+	then
+		return 0
+	else
+		return 1
+	fi
+}
+
+totalWages=0
+count=0
+
+getWorkingHours(){
+	wage=$1
+	while((count!=20))
+	do
+		Attendance
+		result=$?
+		if(($result==1))
+		then
+			dailyWage[$count]=$wage
+		else
+			dailyWage[$count]=0
+		fi
+		count=$((count+1))
+	done
+}
+
+getWorkingHours 160
+
+echo "Daily Wage"
+for((i=0;i<20;i++))
+do
+	printf  "Day %02d: ${dailyWage[$i]}\n" "$((i+1))"
+done
